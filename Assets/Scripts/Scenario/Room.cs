e1197ed2cc1d7537e76765ea1c31a5169a19aac2
InstaPizza <ovanzanten@gmail.com>
2018-05-11 13:48:25 +0200

    Better generation

--- Assets/Scripts/Scenario/Room.cs
+++ Assets/Scripts/Scenario/Room.cs
@@ -127,7 +127,7 @@ namespace Assets.Scripts.Scenario
             Transform frame = Door.instance.transform.parent;
 
             int side = RNG.Next(0, 2);
-        //    Debug.Log(side);
+            //    Debug.Log(side);
             switch (side)
             {
                 case 0: //rightside hinge
@@ -263,31 +263,85 @@ namespace Assets.Scripts.Scenario
             }
             furnitureList = new List<GameObject>();
 
+            bool[] placed = new bool[locations.Length];
+
             for (int x = 0; x < Width; x++)
             {
                 for (int y = 0; y < Length; y++)
                 {
-                    Transform current = locations[x + y * Width];
+                    int arrayPosition = x + y * Width;
+                    Transform current = locations[arrayPosition];
 
                     if (current == null) continue;
+                    if (placed[arrayPosition]) continue;
 
-                    if (10 % RNG.Next(1, 15) == 0)
+                    switch (RNG.Next(0,5))
                     {
-                        GameObject furniture = FurnitureArray[RNG.Next(1, FurnitureArray.Length)];
+                        case 0: case 1: case 2:
+                            if (x == 0 || x == Width - 1 || y == 0 || y == Length - 1)
+                            {
+                                switch (RNG.Next(0,5)) {
+                                    case 0: case 1: case 2: 
+                                        SpawnFurniture(FurnitureArray[0], current, 0);
+                                        placed[arrayPosition] = true;
+                                        break;
+                                    case 3:
+                                        SpawnFurniture(FurnitureArray[3], current, 0);
+                                        placed[arrayPosition] = true;
+                                        break;
+                                    case 4:
+                                        SpawnFurniture(FurnitureArray[4], current, 0);
+                                        placed[arrayPosition] = true;
+                                        break;
+                                }
+                               
+                            }
+                            break;
+                        case 3:
+                            if (x > 0 && x < Width - 1 && y > 0 && y < Length - 1 &&
+                                !placed[(x + 1) + (y) * Width] && /*!placed[(x + 1) + (y + 1) * Width] && */
+                                !placed[(x) + (y + 1) * Width] && /*!placed[(x - 1) + (y + 1) * Width] &&*/
+                                !placed[(x - 1) + (y) * Width] && /*!placed[(x - 1) + (y - 1) * Width] &&*/
+                                !placed[(x) + (y - 1) * Width]/* && !placed[(x - 1) + (y - 1) * Width]*/)
+                            {
+                                SpawnFurniture(FurnitureArray[1], current, 0);
+                                placed[arrayPosition] = true;
 
-                        if (x == 0 || x == Width - 1 || y == 0 || y == Length - 1)
-                        {
-                            furniture = FurnitureArray[0];
-                        }
-
-                        furniture.transform.position = current.position;
-
-                        //Debug.Log(current.rotation.eulerAngles);
-                        furniture.transform.rotation = current.rotation;
-                        furniture.transform.Translate( Vector3.forward * 0.37f);
-                        furnitureList.Add(Instantiate(furniture));
+                                SpawnChair((x + 1), y, placed, locations, 90);
+                                SpawnChair((x - 1), y, placed, locations, 270);
+                                SpawnChair((x), (y + 1), placed, locations, 0);
+                                SpawnChair((x), (y - 1), placed, locations, 180);
+                            }
+                            else
+                            {
+                             //   SpawnFurniture(FurnitureArray[4], current, 0);
+                               // placed[arrayPosition] = true;
+                            }
+                            break;
                     }
                 }
+            }
+        }
+
+        public void SpawnFurniture(GameObject furniture, Transform current, int rotation)
+        {
+            furniture.transform.position = current.position;
+            furniture.transform.rotation = current.rotation * Quaternion.Euler(0, rotation, 0);
+            furnitureList.Add(Instantiate(furniture));
+        }
+
+        public void SpawnChair(int x, int y, bool[] placed, Transform[] locations, int rotation)
+        {
+            int pos = x + y * Width;
+            if (pos < Width * Length)
+            {
+                if (8 % RNG.Next(1, 8) != 0) return;
+                if (placed[pos]) return;
+                if (locations[pos] == null) return;
+                if (locations[pos].rotation.eulerAngles.y > 0) rotation = 0;
+
+                SpawnFurniture(FurnitureArray[2], locations[pos], rotation);
+                placed[pos] = true;
             }
         }
 
@@ -380,7 +434,7 @@ namespace Assets.Scripts.Scenario
                     tempObj.transform.rotation = Quaternion.Euler(new Vector3(0, objectSpawn.rotation, 0));
                     tempObj.SetActive(true);
 
-                    if (!((x == 0 || x == Width - 1 || y == 0 || y == Length - 1) && (x == spawnPos.x && y == spawnPos.z)))
+                    if (!(x == spawnPos.x && y == spawnPos.z))
                     {
                         transforms[x + y * Width] = (tempObj.transform);
                     }
